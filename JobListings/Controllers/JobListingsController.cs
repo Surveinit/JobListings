cc3db2aa928f91a2b9cfc58d53ef4006b286e1f4
Surve <surve8729@gmail.com>
2025-07-20 18:23:28 +0530

    Create JobListing Controller

---
+++ JobListings/Controllers/JobListingsController.cs
@@ -0,0 +1,157 @@
+using JobListings.Data;
+using JobListings.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobListings.Controllers;
+
+public class JobListingsController : Controller
+{
+   private readonly ApplicationDbContext _context;
+
+   public JobListingsController(ApplicationDbContext context)
+   {
+      _context = context;
+   }
+   
+   // GET: JobListings
+   public async Task<IActionResult> Index()
+   {
+      var jobListings = await _context.JobListings
+         .Where(j => j.IsActive)
+         .OrderByDescending(j => j.PostedDate)
+         .ToListAsync();
+      
+      return View(jobListings);
+   }
+   
+   // GET: JobListings/Details/5
+   public async Task<IActionResult> Details(int? id)
+   {
+      if (id == null)
+      {
+         return NotFound();
+      }
+
+      var jobListing = await _context.JobListings
+         .FirstOrDefaultAsync(m => m.Id == id);
+
+      if (jobListing == null)
+      {
+         return NotFound();
+      }
+      
+      return View(jobListing);
+   }
+   
+   // GET: JobListings/Create
+   public IActionResult Create()
+   {
+      return View();
+   }
+   
+   // POST: JobListings/Create
+   [HttpPost]
+   [ValidateAntiForgeryToken]
+   public async Task<IActionResult> Create(
+      [Bind("Title, Company, Location, Description, Salary, JobType")] JobListing jobListing)
+   {
+      if (ModelState.IsValid)
+      {
+         jobListing.PostedDate = DateTime.Now;
+         jobListing.IsActive = true;
+         
+         _context.Add(jobListing);
+         await _context.SaveChangesAsync();
+      }
+      return View(jobListing);
+   }
+   
+   // GET: JobListings/Edit/2
+   public async Task<IActionResult> Edit(int? id)
+   {
+      if (id == null)
+      {
+         return NotFound();
+      }
+
+      var jobListing = await _context.JobListings.FindAsync(id);
+      if (jobListing == null)
+      {
+         return NotFound();
+      }
+      return View(jobListing);
+   }
+   
+   // POST: JobListing/Edit/2
+   [HttpPost]
+   [ValidateAntiForgeryToken]
+   public async Task<IActionResult> Edit(int id,
+      [Bind("Id, Title, Company, Location, Description, Salary, PostedDate, JobType, IsActive")] JobListing jobListing)
+   {
+      if (id != jobListing.Id)
+      {
+         return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+         try
+         {
+            _context.Update(jobListing);
+            await _context.SaveChangesAsync();
+         }
+         catch(DbUpdateConcurrencyException)
+         {
+            if (!JobListingExists(jobListing.Id))
+            {
+               return NotFound();
+            }
+            else
+            {
+               throw;
+            }
+         }
+
+         return RedirectToAction(nameof(Index));
+      }
+      return View(jobListing);
+   }
+   
+   // GET: JobListings/Delete/5
+   public async Task<IActionResult> Delete(int? id)
+   {
+      if (id == null)
+      {
+         return NotFound();
+      }
+
+      var jobListing = await _context.JobListings.FirstOrDefaultAsync(m => m.Id == id);
+      if (jobListing == null)
+      {
+         return NotFound();
+      }
+      return View(jobListing);
+   }
+   
+   // POST: JobListings/Delete/5
+   [HttpPost, ActionName("Delete")]
+   [ValidateAntiForgeryToken]
+   public async Task<IActionResult> DeleteConfirmed(int id)
+   {
+      var jobListing = await _context.JobListings.FindAsync(id);
+      if (jobListing != null)
+      {
+         _context.JobListings.Remove(jobListing);
+         await _context.SaveChangesAsync();
+      }
+
+      return RedirectToAction(nameof(Index));
+
+   }
+
+   public bool JobListingExists(int id)
+   {
+      return _context.JobListings.Any(e => e.Id == id);
+   }
+}
